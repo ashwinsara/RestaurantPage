ed7c0dfd699bf2febac9b7056f289a17d0efd6f6
Ashwin_Kumar <ashwinkumar.dsce@gmail.com>
2023-08-07 21:41:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/Login';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h1 className="Style-header-Name">
+            HOTEL SPECIAL HIGHWAY DHABA
+          </h1>
+        <div><Login/></div>
+
+        <h2>
+        <div>
+        <ul className="App-header-Services">
+        <li class>
+        <a href="https://www.w3schools.com">Home</a>
+        </li>
+        <li class>
+        <a href="https://www.w3schools.com">About Us</a>
+        </li>
+        <li class>
+        <a href="https://www.w3schools.com">Gallery</a>
+        </li>
+        <li class>
+        <a href="https://www.w3schools.com">Catering</a>
+        </li>
+        <li class>
+        <a href="https://www.w3schools.com">Career</a>
+        </li>
+        <li class>
+        <a href="https://www.w3schools.com">Contact Us</a>
+        </li>
+        </ul>
+        </div>
+        </h2>
+        
+        </div>
+      </header>
+    
+
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Add your login logic here (e.g., API call, authentication)
+
+    // For this example, we will just log the user's credentials
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
